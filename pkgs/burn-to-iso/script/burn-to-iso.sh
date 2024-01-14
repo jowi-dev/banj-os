119eda0d276f09c9a8cea278d651246f2a06aee7
jowi-dev <joey8williams@gmail.com>
2024-01-14 18:20:45 -0500

    more installer

--- pkgs/burn-to-iso/script/burn-to-iso.sh
+++ pkgs/burn-to-iso/script/burn-to-iso.sh
@@ -1,10 +1,18 @@
+  # TODO - if more than one config is generated give a choose here
+HARDCODED_ISO_PATH=~/.config/nix-config/result/iso/nixos.iso
+
+
 echo "Choose device to burn ISO to"
-device=$(gum choose $(lsblk --noheadings -o NAME))
-sudo umount /dev/$device
-burn_new=$(gum confirm "Burn new ISO?" && gum spin --spinner dot --title "Generating ISO from nix config" -- nixos-generate -f iso --flake ~/.config/nix-config/ || echo "Skipping ISO creation")
-# TODO - if more than one config is generated give a choose here
-iso=~/.config/nix-config/result/iso/nixos.iso
+device=$(gum choose $(lsblk -p -S --noheadings -o NAME) "NONE")
+if [ $device != "NONE" ]; then
+  sudo umount $device
 
-gum confirm "Burn $iso to $device ?" && gum spin --spinner dot --title "Burning to $device" -- sudo dd bs=4M if=$iso of=/dev/$device status=progress oflag=sync || echo "Discarded iso"
+  gum confirm "Format USB Drive and clear contents?" && sudo mkfs.vfat -F 32 $device || echo "Skipping USB formatting"
 
+  gum confirm "Burn new ISO?" && gum spin --spinner dot --title "Generating ISO from nix config" -- nixos-generate -f iso --flake ~/.config/nix-config/ || echo "Skipping ISO creation"
 
+  gum confirm "Burn $HARDCODED_ISO_PATH to $device ?" && gum spin --spinner dot --title "Burning to $device" -- sudo dd bs=4M if=$HARDCODED_ISO_PATH of=$device status=progress oflag=sync || echo "Discarded iso"
+
+else
+  echo "No Flashable devices mounted."
+fi
