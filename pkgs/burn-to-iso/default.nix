d98bf72355727ff61cd6b03a751d3946f6d5d374
jowi-dev <joey8williams@gmail.com>
2024-01-14 18:20:45 -0500

    more installer

--- pkgs/burn-to-iso/default.nix
+++ pkgs/burn-to-iso/default.nix
@@ -1,16 +1,19 @@
 # nix-build -E 'with import <nixpkgs> { }; callPackage ./default.nix { }'
 
-{ stdenv, pkgs, ... }:
+{ stdenv, pkgs, lib, makeWrapper, ... }:
   stdenv.mkDerivation {
     pname = "burn-to-iso";
     version = "1.0.0";
-    src = ./script/burn-to-iso.sh;
-    buildInputs = [ gum nixos-generators ];
-    #nativeBuildInputs = [ makeWrapper ];
+    src = ./script;
+    buildInputs = [ 
+      pkgs.gum 
+      pkgs.nixos-generators 
+    ];
+    nativeBuildInputs = [ makeWrapper ];
     installPhase = ''
       mkdir -p $out/bin
-      cp burn-to-iso.sh $out/bin/burn-to-iso.sh
+      cp burn-to-iso.sh $out/bin/burn-to-iso
+      wrapProgram $out/bin/burn-to-iso \
+        --prefix PATH : ${lib.makeBinPath [ pkgs.gum pkgs.nixos-generators ]}
     '';
-#      wrapProgram $out/bin/github-downloader.sh \
-#        --prefix PATH : ${lib.makeBinPath [ bash subversion ]}
   }
