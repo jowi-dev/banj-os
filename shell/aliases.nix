54eea3e02b78287a2d095a233f1a4a7145c7ccb9
Joe Williams <jwilliams@joinpapa.com>
2024-02-06 10:20:48 -0500

    make new alias

--- shell/aliases.nix
+++ shell/aliases.nix
@@ -3,8 +3,10 @@
   port-kill="kill $(lsof -t -i:4369)";
 
   garbage-collect="nix-collect-garbage";
-  flake-move-out="mv flake.nix flake.lock env/default.nix ~/";
-  flake-move-in="mv ~/flake.nix ~/flake.lock ./ && ~/default.nix ./env/";
+  flake-move-out="mv flake.nix flake.lock ~/";
+  env-move-out="mv env/default.nix ~/";
+  flake-move-in="mv ~/flake.nix ~/flake.lock ./";
+  env-move-in="mv ~/default.nix ./env/";
   flake-hide="git add --intent-to-add flake.nix flake.lock env/default.nix && git update-index --assume-unchanged flake.nix flake.lock env/default.nix";
 
   bs="echo ':terminal' | nvim -s --listen /tmp/$(tmux display-message -p '#S').pipe";
