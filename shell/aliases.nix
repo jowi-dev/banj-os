879c85e70b38690f73e4a816730aff3c5ae6d534
Joe Williams <jwilliams@joinpapa.com>
2024-02-12 08:39:17 -0500

    modify aliases

--- shell/aliases.nix
+++ shell/aliases.nix
@@ -12,7 +12,7 @@
   env-move-out="mv env/default.nix ~/";
   flake-move-in="mv ~/flake.nix ~/flake.lock ./";
   env-move-in="mv ~/default.nix ./env/";
-  flake-hide="git add --intent-to-add flake.nix flake.lock env/default.nix && git update-index --assume-unchanged flake.nix flake.lock env/default.nix";
+  flake-hide="git add --intent-to-add flake.nix flake.lock && git update-index --assume-unchanged flake.nix flake.lock";
 
   bs="echo ':terminal' | nvim -s --listen /tmp/$(tmux display-message -p '#S').pipe";
 }
